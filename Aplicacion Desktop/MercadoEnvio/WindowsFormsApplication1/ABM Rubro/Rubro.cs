720709f1e0faf7fc84b31642eb8855ad6fc905e0
fecheromero <fecheromero@gmail.com>
2016-05-25 22:41:10 -0300

    creada la ventana del abm de rubro + ventana listado

---
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/ABM Rubro/Rubro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.ABM_Rubro
+{
+    public partial class Rubro : Form
+    {
+        public Rubro()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listadosToolStripMenuItem_DoubleClick(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Alta alta = new Alta();
+            this.Hide();
+            alta.ShowDialog();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Listado listado = new Listado();
+            this.Hide();
+            listado.ShowDialog();
+            this.Close();
+        }
+    }
+}
--- Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Program.cs
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Program.cs
@@ -17,6 +17,8 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Inicio());
+            Application.Run(new ABM_Rubro.Rubro());
+           
         }
     }
 }
